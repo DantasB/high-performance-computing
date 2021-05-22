40b9a715a9b52c5166601382f8f87be39406b3cb
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-22 12:27:40 -0300

    Updating bash file

--- Third_Exercise/runner.sh
+++ Third_Exercise/runner.sh
@@ -1,16 +1,24 @@
 export OMP_NUM_THREADS=2
 
-CPP_PATH=cpp_files
+CPP_DIR=cpp_files
+INPUT_DIR=input_file
 PROGRAM=laplace_openmp.cxx
+INPUT_FILE=input.txt
 EXECUTOR=runner
-PROGRAM_PATH=$CPP_PATH/$PROGRAM
-
+PROGRAM_PATH=$CPP_DIR/$PROGRAM
+INPUT_PATH=$INPUT_DIR/$INPUT_FILE
 
 g++ $PROGRAM_PATH -fopenmp -o $EXECUTOR 
 for counter in {1..6};
 do
     export OMP_NUM_THREADS=$counter
-    echo $(./$EXECUTOR)
+    for nx in 512 1024 2048;
+    do
+        mkdir -p $INPUT_DIR
+        echo $nx 1000 0.00000000000000001 > $INPUT_PATH
+        echo $(./$EXECUTOR < $INPUT_PATH)
+        rm $INPUT_PATH
+    done
 done
 
 rm $EXECUTOR

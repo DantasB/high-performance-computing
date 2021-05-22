3aee4b6b35c6a81989bb15426bb2edcb0befd4fc
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-22 14:37:45 -0300

    Adding other types of running on bash

--- Third_Exercise/runner.sh
+++ Third_Exercise/runner.sh
@@ -1,26 +1,67 @@
-export OMP_NUM_THREADS=2
+INPUT_DIR=input_file
+INPUT_FILE=input.txt
 
 CPP_DIR=cpp_files
-INPUT_DIR=input_file
 BASE_PROGRAM=laplace.cxx
 OPTIMIZED_PROGRAM=laplace_openmp.cxx
-INPUT_FILE=input.txt
+
+OUTPUT_DIR=output_files
 EXECUTOR=runner
 
 BASE_PROGRAM_PATH=$CPP_DIR/$BASE_PROGRAM
 OPTIMIZED_PROGRAM_PATH=$CPP_DIR/$OPTIMIZED_PROGRAM
 INPUT_PATH=$INPUT_DIR/$INPUT_FILE
 
-#g++ $BASE_PROGRAM_PATH  -o $EXECUTOR 
-g++ $OPTIMIZED_PROGRAM_PATH -fopenmp -o $EXECUTOR 
-for counter in {1..6};
+MAX_THREADS=$(python -c "import psutil; print(psutil.cpu_count(logical=True))")
+
+g++ $BASE_PROGRAM_PATH -o $EXECUTOR 
+THREAD_DIR=$OUTPUT_DIR/BASE/
+mkdir -p $THREAD_DIR
+for nx in 512 1024 2048;
 do
+    mkdir -p $INPUT_DIR
+    echo $nx 1000 0.000000000000001 > $INPUT_PATH
+    echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+    rm $INPUT_PATH
+done
+
+g++ $BASE_PROGRAM_PATH -O3 -o $EXECUTOR
+THREAD_DIR=$OUTPUT_DIR/BASE_O3/
+mkdir -p $THREAD_DIR
+for nx in 512 1024 2048;
+do
+    mkdir -p $INPUT_DIR
+    echo $nx 1000 0.000000000000001 > $INPUT_PATH
+    echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+    rm $INPUT_PATH
+done
+
+g++ $OPTIMIZED_PROGRAM_PATH -fopenmp -o $EXECUTOR 
+for counter in $(seq 1 $MAX_THREADS);
+do
+    THREAD_DIR=$OUTPUT_DIR/OPENMP/${counter}"_thread(s)"
+    mkdir -p $THREAD_DIR
     export OMP_NUM_THREADS=$counter
     for nx in 512 1024 2048;
     do
         mkdir -p $INPUT_DIR
         echo $nx 1000 0.000000000000001 > $INPUT_PATH
-        echo $(./$EXECUTOR < $INPUT_PATH)
+        echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+        rm $INPUT_PATH
+    done
+done
+
+g++ $OPTIMIZED_PROGRAM_PATH -O3 -fopenmp -o $EXECUTOR 
+for counter in $(seq 1 $MAX_THREADS);
+do
+    THREAD_DIR=$OUTPUT_DIR/"OPENMP_O3"/${counter}"_thread(s)"
+    mkdir -p $THREAD_DIR
+    export OMP_NUM_THREADS=$counter
+    for nx in 512 1024 2048;
+    do
+        mkdir -p $INPUT_DIR
+        echo $nx 1000 0.000000000000001 > $INPUT_PATH
+        echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
         rm $INPUT_PATH
     done
 done

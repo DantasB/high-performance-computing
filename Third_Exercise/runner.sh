dbf1b7f06affc270a7ebe9b943c53957799b786a
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-05-23 11:43:07 -0300

    Now it writes a single csv file
    All the time, results and other informations are compiled in the same file

--- Third_Exercise/runner.sh
+++ Third_Exercise/runner.sh
@@ -5,7 +5,7 @@ CPP_DIR=cpp_files
 BASE_PROGRAM=laplace.cxx
 OPTIMIZED_PROGRAM=laplace_openmp.cxx
 
-OUTPUT_DIR=output_files
+OUTPUT=output_file.csv
 EXECUTOR=runner
 
 BASE_PROGRAM_PATH=$CPP_DIR/$BASE_PROGRAM
@@ -14,39 +14,35 @@ INPUT_PATH=$INPUT_DIR/$INPUT_FILE
 
 MAX_THREADS=$(python -c "import psutil; print(psutil.cpu_count(logical=True))")
 
+echo 'Result;Nx;Time(s);Flags;Thread(s)' > $OUTPUT
+
 g++ $BASE_PROGRAM_PATH -o $EXECUTOR 
-THREAD_DIR=$OUTPUT_DIR/BASE/
-mkdir -p $THREAD_DIR
 for nx in 512 1024 2048;
 do
     mkdir -p $INPUT_DIR
-    echo $nx 1000 0.000000000000001 > $INPUT_PATH
-    echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+    echo $nx 1000 0.000000000000001 "None" > $INPUT_PATH
+    echo $(./$EXECUTOR < $INPUT_PATH >> $OUTPUT)
     rm $INPUT_PATH
 done
 
 g++ $BASE_PROGRAM_PATH -O3 -o $EXECUTOR
-THREAD_DIR=$OUTPUT_DIR/BASE_O3/
-mkdir -p $THREAD_DIR
 for nx in 512 1024 2048;
 do
     mkdir -p $INPUT_DIR
-    echo $nx 1000 0.000000000000001 > $INPUT_PATH
-    echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+    echo $nx 1000 0.000000000000001 "O3" > $INPUT_PATH
+    echo $(./$EXECUTOR < $INPUT_PATH >> $OUTPUT)
     rm $INPUT_PATH
 done
 
 g++ $OPTIMIZED_PROGRAM_PATH -fopenmp -o $EXECUTOR 
 for counter in $(seq 1 $MAX_THREADS);
 do
-    THREAD_DIR=$OUTPUT_DIR/OPENMP/${counter}"_thread(s)"
-    mkdir -p $THREAD_DIR
     export OMP_NUM_THREADS=$counter
     for nx in 512 1024 2048;
     do
         mkdir -p $INPUT_DIR
-        echo $nx 1000 0.000000000000001 > $INPUT_PATH
-        echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+        echo $nx 1000 0.000000000000001 "fopenmp" > $INPUT_PATH
+        echo $(./$EXECUTOR < $INPUT_PATH >> $OUTPUT)
         rm $INPUT_PATH
     done
 done
@@ -54,14 +50,12 @@ done
 g++ $OPTIMIZED_PROGRAM_PATH -O3 -fopenmp -o $EXECUTOR 
 for counter in $(seq 1 $MAX_THREADS);
 do
-    THREAD_DIR=$OUTPUT_DIR/"OPENMP_O3"/${counter}"_thread(s)"
-    mkdir -p $THREAD_DIR
     export OMP_NUM_THREADS=$counter
     for nx in 512 1024 2048;
     do
         mkdir -p $INPUT_DIR
-        echo $nx 1000 0.000000000000001 > $INPUT_PATH
-        echo $(./$EXECUTOR < $INPUT_PATH > $THREAD_DIR/${nx}".txt")
+        echo $nx 1000 0.000000000000001 "O3 and fopenmp"> $INPUT_PATH
+        echo $(./$EXECUTOR < $INPUT_PATH >> $OUTPUT)
         rm $INPUT_PATH
     done
 done
